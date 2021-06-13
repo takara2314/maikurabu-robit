c01cc3b11bc1a78ef8e2240e3b00a9dc2ce554a2
takara2314 <takara2314_TK@outlook.jp>
2021-06-13 12:05:03 +0900

    👨🏻‍🏭 Include embed message

--- onMessage.go
+++ onMessage.go
@@ -7,12 +7,69 @@ import (
 )
 
 func onMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Content == "たからーん" || m.Content == "たからん" {
-		_, err := s.ChannelMessageSend(m.ID, "なーに！")
+	if m.Content == "/robit" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "ロビットです :)")
+
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+		embed := discordgo.MessageEmbed{
+			Title:       "僕について",
+			Description: "やぁ！僕はロビットだよ！マイクラ部のサーバーを管理するよ！",
+			Color:       0x05b5e6,
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL:    "https://github.com/takara2314/maikurabu-robit/raw/main/robit.png",
+				Width:  128,
+				Height: 128,
+			},
+			Author: &discordgo.MessageEmbedAuthor{
+				URL:     "https://github.com/takara2314/maikurabu-robit",
+				Name:    "ロビット",
+				IconURL: "https://github.com/takara2314/maikurabu-robit/raw/main/robit.png",
+			},
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  "/status",
+					Value: "現在のサーバーの情報を表示します。",
+				},
+				{
+					Name:  "開発者 / 管理者",
+					Value: "たからーん (@takara2314)",
+				},
+				{
+					Name:  "開発言語",
+					Value: "Go",
+				},
+			},
+		}
+
+		_, err = s.ChannelMessageSendEmbed(
+			m.ChannelID,
+			&embed,
+		)
 
 		if err != nil {
 			log.Println(err)
 			panic(err)
 		}
 	}
+
+	// if strings.HasPrefix(m.Content, "/status") {
+	// 	embed := discordgo.MessageEmbed{
+	// 		URL:   "test",
+	// 		Title: "test",
+	// 	}
+
+	// 	_, err := s.ChannelMessageSendEmbed(
+	// 		m.ChannelID,
+	// 		&embed,
+	// 	)
+
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		panic(err)
+	// 	}
+	// }
 }
